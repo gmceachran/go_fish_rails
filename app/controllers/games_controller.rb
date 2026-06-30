b1ffeb87a85ce34ff52a5ea85f1041b0f96fb7b5
Gabriel McEachran <gabemceachran@gmail.com>
2026-06-30 08:21:45 -0400

    Add history and stats static pages

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,22 @@ class GamesController < ApplicationController
   end
 
   def history
+    @game_history = [
+      {
+        title: "Game 1",
+        player_count: 3,
+        won: false
+      },
+      {
+        title: "Game 2",
+        player_count: 5,
+        won: true
+      },
+      {
+        title: "Game 3",
+        player_count: 2,
+        won: false
+      }
+    ]
   end
 end
