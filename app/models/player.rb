b67bbda00b39949f714f1b326d38200788459bca
Gabriel McEachran <gabemceachran@gmail.com>
2026-07-04 10:11:42 -0400

    Add validation to player model so that only one player per game can be declared winner

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,15 @@ class Player < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  validates :game_id, uniqueness: { scope: :user, message: "You already joined this game." }
+  validates :game_id, uniqueness: { scope: :user_id, message: "You already joined this game." }
+  validate :only_winner?
+
+  private
+
+  def only_winner?
+    return unless winner
+    return if game.players.where(winner: true).where.not(id: id).empty?
+
+    errors.add(:winner, "has already been declared for this game")
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -14,5 +14,25 @@ RSpec.describe Player, type: :model do
       expect(invalid_player).to_not be_valid
       expect(invalid_player.errors.full_messages.to_sentence).to include "You already joined this game."
     end
+
+    context "when there is no other winner in a game" do
+      let(:player) { create :player }
+
+      it "a player set to winner is valid" do
+        expect(player).to be_valid
+      end
+    end
+
+    context "when there is already one winner in a game" do
+      let(:game) { create :game }
+      let(:winner) { create :player, game: game }
+      let(:looser) { create :player, game: game }
+      before { winner.update winner: true }
+
+      it "any other player set to winner is invalid" do
+        looser.update winner: true
+        expect(looser).to be_invalid
+      end
+    end
   end
 end
