fb33e864edf509948588e45feef0f36aef048dbc
Gabriel McEachran <gabemceachran@gmail.com>
2026-07-06 13:17:18 -0400

    Add go_fish column to Game to store go fish state as json

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_04_130019) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_06_154306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "ended_at"
+    t.jsonb "go_fish"
     t.integer "max_players", default: 5, null: false
     t.datetime "started_at"
     t.integer "state", default: 0, null: false
