6a2a92e745bb75454851be36577df8b783c994b0
Gabriel McEachran <gabemceachran@gmail.com>
2026-07-06 13:18:42 -0400

    Add initial load and dump helper class methods to GoFish::Game to handle json assembly and dessembly

--- app/models/go_fish/game.rb
+++ app/models/go_fish/game.rb
@@ -1,5 +1,17 @@
 module GoFish
   class Game
-    
+    attr_reader :game_id
+
+    def initialize(game_id = 1)
+      @game_id = game_id
+    end
+
+    def self.load(json)
+      JSON.parse(json)
+    end
+
+    def self.dump(object)
+      object.to_json
+    end
   end
 end
--- spec/models/go_fish/game_spec.rb
+++ spec/models/go_fish/game_spec.rb
@@ -1,5 +1,21 @@
 require "rails_helper"
 
 RSpec.describe GoFish::Game, type: :model do
-  
+  let(:game) { described_class.new }
+  let(:object) { { "game_id" => 1 } }
+  let(:json) { object.to_json }
+
+  describe "#load" do
+    it "turns the given json string into a ruby hash" do
+      loaded_json = GoFish::Game.load(json)
+      expect(loaded_json).to eq object
+    end
+  end
+
+  describe "#dump" do
+    it "turns the given hash into a json string" do
+      dumped_object = GoFish::Game.dump(object)
+      expect(dumped_object).to eq json
+    end
+  end
 end
